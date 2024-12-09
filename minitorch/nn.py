890716608bb5c1464604be89c134f82ba55ff476
Tung Doan Nguyen <tnguye48@hawk.iit.edu>
2024-12-09 01:48:57 -0500

    Add 4.3

--- minitorch/nn.py
+++ minitorch/nn.py
@@ -35,8 +35,37 @@ def tile(input: Tensor, kernel: Tuple[int, int]) -> Tuple[Tensor, int, int]:
     kh, kw = kernel
     assert height % kh == 0
     assert width % kw == 0
-    # TODO: Implement for Task 4.3.
-    raise NotImplementedError("Need to implement for Task 4.3")
+
+    # New Height and New Width is equal to the folding of the kernel height and width.
+    new_height = height // kh
+    new_width = width // kw
+
+    # Makes the input contiguous and then reshapes it to the new shape through view.
+    input = input.contiguous().view(batch, channel, new_height, kh, new_width, kw)
+    # Using the permute function to rearrange the memory of the tensor, 
+    # so that we can view into the shape we want.
+    input = input.permute(0, 1, 2, 4, 3, 5).contiguous()
+    # View the tensor into the new shape.
+    input = input.view(batch, channel, new_height, new_width, kh * kw)
+    return input, new_height, new_width
 
 
-# TODO: Implement for Task 4.3.
+def avgpool2d(t: Tensor, kernel: Tuple[int, int]) -> Tensor:
+    """Apply average pooling 2D over a tensor t using a given kernel size
+
+    Args:
+        t: Input tensor of size batch x channel x height x width
+        kernel: height x width of pooling
+
+    Returns:
+        Tensor of size batch x channel x new_height x new_width that has been pooled over the original tensor using the tile function.
+
+    """
+    batch, channel, _, _ = t.shape
+    # Reshape the input tensor using the tile function
+    tiled, new_height, new_width = tile(t, kernel)
+    # Once data is returned from Tile, each element in the last dimension is one element for the kernel. 
+    # We just need to the the average of the last dimension to calculate the average pooling.
+    # We then reshape the tensor to the new shape.
+    output = tiled.mean(dim=-1).view(batch, channel, new_height, new_width)
+    return output
